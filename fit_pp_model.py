ad7e0ff39c71da5b8c600438c3dc69fe36d1c385
Carson Eisenach <carson@eisenach.org>
2019-05-29 23:51:37 -0400

    Added some scaffolding for the solver

---
+++ fit_pp_model.py
@@ -0,0 +1,88 @@
+import os
+import logging
+logger = logging.getLogger(__name__)
+import time
+import numpy as np
+from collections import namedtuple
+import yaml
+
+import model
+import utils
+
+
+ModelSpec = namedtuple("ModelSpec",["a"])
+
+def configure_run(run_config):
+    vf_result_path = os.path.join(run_config['odir'],'value_function.npy')
+    make_directory(run_config['odir'])
+
+    with open(os.path.join(run_config['odir'],'run_config.yaml'), 'w') as fp:
+        yaml.dump(run_config, fp, default_flow_style=False)
+
+    log_level = logging.DEBUG if run_config['debug'] else logging.INFO
+    if not run_config['console']:
+        logging.basicConfig(filename=os.path.join(run_config['odir'],'log_run_%d.log' % run_config['run']),
+                level=log_level,
+                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',)
+    else:
+        logging.basicConfig(level=log_level)
+
+
+    configure_run_io(run_config)
+
+    # TODO: Fix this for numba -- I don't think it will seed correctly as is
+    if run_config['seed'] is not None:
+        random.seed(run_config['seed'])
+
+    log_str = '\r\n###################################################\r\n' + \
+              '\tModel: %s\r\n' % run_config['model']['name'] + \
+              '\tMax Updates, Save Interval: (%d,%d) \r\n' % (run_config['num_updates'],run_config['save_interval']) + \
+              '###################################################'
+    logger.info(log_str)
+
+
+
+def fit_model(run_config):
+
+    # Select the algorithm
+    algorithm = None
+    try:
+        algorithm_class = getattr(algorithm,run_config['alg'])
+        algorithm = algorithm_class()
+    except AttributeError as e:
+        raise RuntimeError('Algorithm "%s" not found' % run_config['alg'])
+
+
+    # episode_results = np.array([]).reshape((0,6))
+    cur_update = 0
+    start = time.time()
+    while cur_update < run_config['num_updates']:
+        # batch,crs,trs,els = smp.sample()
+        # algo.update(batch)
+        #
+        # # save episode results
+        # for i,(cr,tr,el) in enumerate(zip(crs,trs,els)):
+        #     finished_episodes += 1
+        #     total_samples = cur_update * samples_per_update
+        #     # stores: total_updates, total_episodes, total_samples, current_episode_length, current_total_reward, current_cumulative_reward
+        #     episode_results = np.concatenate((episode_results,np.array([cur_update,finished_episodes,total_samples,el,tr,cr],ndmin=2)),axis=0)
+        #     np.save(episode_results_path, episode_results)
+        #     logger.info('Update Number: %06d, Finished Episode: %04d ---  Length: %.3f, TR: %.3f, CDR: %.3f'% (cur_update,finished_episodes,el,tr,cr))
+        break
+        # checkpoint
+        if cur_update % run_config['save_interval'] == 0:
+            plc.save_model(os.path.join(run_config['odir'],'model_update_%06d.pt' % (cur_update)))
+
+        cur_update += 1
+
+    end = time.time()
+    logger.info('Time elapsed: %.3f %' (end - start))
+
+
+
+
+if __name__ == "__main__":
+    parser = utils.fit_model_argparser()
+    args = parser.parse_args()
+    run_config = utils.run_config_from_args(args)
+    fit_model(run_config)
--- utils.py
+++ utils.py
@@ -10,3 +10,37 @@ def load_model_from_yaml(file_path):
             print(yaml.safe_load(stream))
         except yaml.YAMLError as exc:
             print(exc)
+
+
+
+def make_directory(dirpath):
+    os.makedirs(dirpath,exist_ok=True)
+
+def run_config_from_args(args):
+    run_config = {'debug' : args.debug,
+                         'console' : args.console,
+                         'odir' : args.odir if args.odir is not None else 'out/experiment_%s' % time.strftime("%Y.%m.%d_%H.%M.%S"),
+                         'model': load_model_from_yaml(args.model),
+                         'seed': args.seed,
+                         'tolerance': args.tolerance,
+                         'max_updates' : int(args.max_updates),
+                         'save_interval' : int(args.log_interval),
+                         'save_interval' : int(args.save_interval)}
+
+    return run_config
+
+
+def fit_model_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--odir", type=str, default='out', help="output directory")
+    parser.add_argument("-d", "--debug", action="store_true", help="debug")
+    parser.add_argument("-u",'--max_updates', type=float, default=1e4, help="max number of iterations")
+    parser.add_argument("-e",'--tolerance', type=float, default=1e-4, help="error tolerance")
+    parser.add_argument("-s",'--save_interval', type=float, default=1e3, help="current estimates save Interval")
+    parser.add_argument("-a",'--algorithm', type=string, default='VFI', help="which algorithm to use")
+    parser.add_argument("-l",'--log_interval', type=int, default=50, help="Log Interval")
+    parser.add_argument("-m",'--model', type=str, default="./model/simplified.yaml", help="Which model to use")
+    parser.add_argument("-co", "--console", action="store_true", help="log to console")
+    parser.add_argument('--seed', type=int, default=543, metavar='N',help='random seed (default: 543). -1 indicates no seed')
+
+    return parser
