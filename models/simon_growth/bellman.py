dfcaa55e68b1c19721b42d40fd0b044fd1b18bb8
Carson Eisenach <carson@eisenach.org>
2019-12-12 14:23:13 -0500

    Remove duplicated code in value function grad calculation

--- models/simon_growth/bellman.py
+++ models/simon_growth/bellman.py
@@ -34,9 +34,7 @@ def V_INFINITY(X=[], params=None):
 #   Objective Function during VFI (note - we need to interpolate on an "old" GPR)
 
 
-def EV_F_ITER(X_t, U, params, gp_old, *args):
-    # import pdb
-    # pdb.set_trace()
+def EV_F_ITER(X_t, U, params, V_tp1, *args):
     # Extract Variables
     cons = U[0:params.n_agents]
     lab = U[params.n_agents:2 * params.n_agents]
@@ -53,7 +51,7 @@ def EV_F_ITER(X_t, U, params, gp_old, *args):
     Xtest[0, :] = X_tp1_cube
 
     # interpolate the function, and get the point-wise std.
-    V_old, sigma_test = gp_old.predict(Xtest, return_std=True)
+    V_old, sigma_test = V_tp1.predict(Xtest, return_std=True)
 
     VT_sum = dynamics.utility(cons, lab, params) + params.beta * V_old
 
@@ -63,10 +61,13 @@ def EV_F_ITER(X_t, U, params, gp_old, *args):
 #=======================================================================
 #   Computation of gradient (first order finite difference) of initial objective function
 
-
 def EV_GRAD_F(X_t, U, params, *args):
-    # import pdb
-    # pdb.set_trace()
+    return _EV_GRAD_F_IMPL(X_t,U,params,EV_F)
+
+def EV_GRAD_F_ITER(X_t, U, params, V_tp1, *args):
+    return _EV_GRAD_F_IMPL(X_t,U,params,EV_F_ITER,V_tp1)
+
+def _EV_GRAD_F_IMPL(X_t, U, params, _EV_F, V_tp1 = None):
     N = len(U)
     GRAD = np.zeros(N, float)  # Initial Gradient of Objective Function
     h = 1e-4
@@ -76,57 +77,22 @@ def EV_GRAD_F(X_t, U, params, *args):
 
         if (uAdj[iuN] - h >= 0):
             uAdj[iuN] = U[iuN] + h
-            fu2 = EV_F(X_t, uAdj, params)
+            fu2 = _EV_F(X_t, uAdj, params, V_tp1)
 
             uAdj[iuN] = U[iuN] - h
-            fu1 = EV_F(X_t, uAdj, params)
+            fu1 = _EV_F(X_t, uAdj, params, V_tp1)
 
             GRAD[iuN] = (fu2 - fu1) / (2.0 * h)
 
         else:
             uAdj[iuN] = U[iuN] + h
-            fu2 = EV_F(X_t, uAdj, params)
+            fu2 = _EV_F(X_t, uAdj, params, V_tp1)
 
             uAdj[iuN] = U[iuN]
-            fu1 = EV_F(X_t, uAdj, params)
+            fu1 = _EV_F(X_t, uAdj, params, V_tp1)
             GRAD[iuN] = (fu2 - fu1) / h
 
     return GRAD
-
-
-#=======================================================================
-#   Computation of gradient (first order finite difference) of the objective function
-
-
-def EV_GRAD_F_ITER(X_t, U, params, gp_old, *args):
-    # import pdb
-    # pdb.set_trace()
-    N = len(U)
-    GRAD = np.zeros(N, float)  # Initial Gradient of Objective Function
-    h = 1e-4
-
-    for iuN in range(N):
-        uAdj = np.copy(U)
-
-        if (uAdj[iuN] - h >= 0):
-            uAdj[iuN] = U[iuN] + h
-            fu2 = EV_F_ITER(X_t, uAdj, params, gp_old)
-
-            uAdj[iuN] = U[iuN] - h
-            fu1 = EV_F_ITER(X_t, uAdj, params, gp_old)
-
-            GRAD[iuN] = (fu2 - fu1) / (2.0 * h)
-
-        else:
-            uAdj[iuN] = U[iuN] + h
-            fu2 = EV_F_ITER(X_t, uAdj, params, gp_old)
-
-            uAdj[iuN] = U[iuN]
-            fu1 = EV_F_ITER(X_t, uAdj, params, gp_old)
-            GRAD[iuN] = (fu2 - fu1) / h
-
-    return GRAD
-
 
 def sparsity_hess(N):
     NZ = (N**2 - N) // 2 + N
