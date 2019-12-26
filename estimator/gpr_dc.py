Cleanup and fixes. Switch to using scipy numerical differentiation subroutine

estimator/gpr_dc.py
@@ -20,9 +20,10 @@ class GPR_DC:
             return np.amax(all_preds,axis=1).reshape(-1,)
         if k is not None:
             return self._impl[k].predict(X,**kwargs)
-        
-        raise RuntimeError('Should not reach here....')
-        return self._all_preds(X,k,**kwargs)
+
+        res = self._all_preds(X,k,**kwargs)
+        res = res.reshape(-1,) # reshape to make sure 1D array
+        return res
 
     def fit(self,X,y):
         assert y.shape[1] == self.num_choices, "Y should be array with width num_choices"

models/dc_simple/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.optimize as opt
 
 from solver import IPOptCallback
 from . import bellman, dynamics
@@ -21,7 +22,8 @@ class DCSimple:
         assert U_k is not None, "Must specify discrete action"
         V_tp1 = bellman.V_T if V_tp1 is None else V_tp1
         F = lambda U: bellman.state_action_value(X, U, U_k, self.params, V_tp1)
-        return mu.derivative_fd(F,U)
+        res = opt.approx_fprime(U,F,1e-8)
+        return res
 
     @property
     def value_hess_sparsity(self):
@@ -33,7 +35,7 @@ class DCSimple:
     # returns derivative (jacobian) of constraint set
     def constraints_deriv(self, X, U, **kwargs):
         F = lambda U: dynamics.EV_G(X,U,self.params)
-        return mu.jacobian_fd(F, U, self.dim.control,self.dim.constraints)
+        return opt.approx_fprime(U,F,1e-8)
 
     @property
     def constraints_deriv_sparsity(self):

models/dc_simple/dynamics.py
@@ -7,15 +7,14 @@ def transition(X_t, U_t, U_k, params=None):
     return X_tp1
 
 
-def EV_G(X_t, U, params):
+def EV_G(X_t, U, params,constraint=None):
+    if constraint is not None:
+        return X_t[constraint] - U[constraint]
+
     N = len(U)
     M = N
     G = np.empty(M, float)
-    
     G = X_t - U
-    if utils.test_inf_nan(G):
-        import pdb; pdb.set_trace()
-
     return G
 
 

solver/solver_ipopt.py
@@ -52,7 +52,7 @@ def solve(model, X, **kwargs):
             acceptable_tol=acceptable_tol,
             derivative_test='first-order',
             hessian_approximation="limited-memory",
-            print_level=12)
+            print_level=0)
 
     z_l = np.zeros(N)
     z_u = np.zeros(N)

utils/math/differentiate.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import scipy.optimize as opt
 
 def test_inf_nan(val):
     if sum(np.isinf(val)) > 0 or sum(np.isnan(val)) > 0:
@@ -30,25 +30,26 @@ def derivative_fd(F,X,h = 1e-4):
     return D
 
 
-def jacobian_fd(F,X,N,M):
-    assert N == len(X), "X is not same dimension as N"
+def jacobian_fd(F,X,M,eps=1e-8):
     N = len(X)
     NZ = M * N
     D = np.empty(NZ, float)
 
     # Finite Differences
-    h = 1e-4
-    F1 = F(X)
 
     for ixM in range(M):
-        for ixN in range(N):
-            dX = np.copy(X)
-            dX[ixN] = dX[ixN] + h
-            F2 = F(dX)
-            D[ixN + ixM * N] = (F2[ixM] - F1[ixM]) / h
+        F_i = lambda x : F(x,constraint=ixM)
+        D[ixM*N:(ixM+1)*N] = approx_fprime(X,F_i, eps)
 
-    if test_inf_nan(D):
-        import pdb; pdb.set_trace()
+    # for ixM in range(M):
+    #     for ixN in range(N):
+    #         dX = np.copy(X)
+    #         dX[ixN] = dX[ixN] + h
+    #         F2 = F(dX)
+    #         D[ixN + ixM * N] = (F2[ixM] - F1[ixM]) / h
+
+    # if test_inf_nan(D):
+    #     import pdb; pdb.set_trace()
 
     return D
 