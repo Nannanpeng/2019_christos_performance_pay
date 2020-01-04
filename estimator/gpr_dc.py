d3e9b3836ee73626f56c9abc96888ba5a1d0fde4
Carson Eisenach <carson@eisenach.org>
2020-01-03 21:59:41 -0500

    Misc fixes

--- estimator/gpr_dc.py
+++ estimator/gpr_dc.py
@@ -8,6 +8,7 @@ import collections
 
 import third_party.hjmshi.lbfgs as opt_tp
 
+
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
@@ -29,7 +30,29 @@ class SpectralMixtureGPModel(gpytorch.models.ExactGP):
         self.mean_module = gpytorch.means.ZeroMean()
         self.covar_module = gpytorch.kernels.SpectralMixtureKernel(
             num_mixtures=4)
-        # self.covar_module.initialize_from_data(train_x, train_y)
+        self._initialize()
+
+    # TODO: Is this a reasonable init??
+    def _initialize(self):
+        cmsd = self.covar_module.state_dict()
+        init_sd = {
+            'covar_module.raw_mixture_means': torch.empty(cmsd['raw_mixture_means'].shape).normal_(),
+            'covar_module.raw_mixture_scales': torch.empty(cmsd['raw_mixture_scales'].shape).normal_()
+        }
+        init_sd = collections.OrderedDict(init_sd)
+        self.load_state_dict(init_sd,strict=False)
+
+    def covariance_state_string(self):
+        log_str = """
+########## Covariance Module State #########
+Raw Mixture Weights: %s
+Raw Mixture Scales: %s
+Raw Mixture Means: %s
+"""
+        cm = self.covar_module.state_dict()
+        return log_str % (cm['raw_mixture_weights'],
+                          cm['raw_mixture_means'][:, 0, 0],
+                          cm['raw_mixture_scales'][:, 0, 0])
 
     def forward(self, x):
         mean_x = self.mean_module(x)
@@ -72,8 +95,8 @@ class GPR_DC:
         self.num_choices = num_choices
         self._impl = [None] * self.num_choices
         self._data = [None] * self.num_choices
-        self._xnorm = [None,None]
-        self._ynorm = ([None] * self.num_choices,[None] * self.num_choices)
+        self._xnorm = [None, None]
+        self._ynorm = ([None] * self.num_choices, [None] * self.num_choices)
 
     def save(self, path):
         val = self.state_dict()
@@ -99,7 +122,10 @@ class GPR_DC:
             self._impl[k](X, **kwargs).mean.unsqueeze(-1)
             for k in range(self.num_choices)
         ]
-        preds_scaled = [self._ynorm[1][k]*p + self._ynorm[0][k] for k,p in enumerate(preds)]
+        preds_scaled = [
+            self._ynorm[1][k] * p + self._ynorm[0][k]
+            for k, p in enumerate(preds)
+        ]
         preds_scaled = torch.cat(preds_scaled, -1)
         return preds_scaled
 
@@ -112,7 +138,7 @@ class GPR_DC:
             return max_preds
         if k is not None:
             res = self._impl[k](X, **kwargs).mean
-            return self._ynorm[1][k]*res + self._ynorm[0][k]
+            return self._ynorm[1][k] * res + self._ynorm[0][k]
 
         res = self._all_preds(X, **kwargs)
         return res
@@ -127,7 +153,8 @@ class GPR_DC:
         for i in range(self.num_choices):
             _X = X
             _y = y[:, i]
-            likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.GreaterThan(0.1))
+            likelihood = gpytorch.likelihoods.GaussianLikelihood(
+                noise_constraint=gpytorch.constraints.GreaterThan(0.1))
             idxs = np.logical_not(np.isnan(_y))
             _y = torch.tensor(_y[idxs])
             self._ynorm[0][i] = _y.mean(dim=0)
@@ -135,7 +162,7 @@ class GPR_DC:
             _X = _X[idxs]
             _y = (_y - self._ynorm[0][i]) / self._ynorm[1][i]
             self._data[i] = (_X, _y)
-            self._impl[i] = _fit_gpr(GPR_TYPE, likelihood, _X, _y,**kwargs)
+            self._impl[i] = _fit_gpr(GPR_TYPE, likelihood, _X, _y, **kwargs)
 
     def __str__(self):
         fstr = '\tDC%d: %s'
@@ -146,25 +173,37 @@ class GPR_DC:
         return 'GPR_DC(%d):\r\n%s' % (self.num_choices, strs)
 
 
-def _fit_gpr(GPR_TYPE, likelihood, X, y, num_iter=1000, lr=1, debug_log_interval=5, num_restarts = 9):
-    logger.info('Fiting GPR (num_iter: %d, num_restarts: %d)' % (num_iter, num_restarts))
+def _fit_gpr(GPR_TYPE,
+             likelihood,
+             X,
+             y,
+             num_iter=500,
+             lr=1,
+             debug_log_interval=5,
+             num_restarts=9):
+    logger.info('Fiting GPR (num_iter: %d, num_restarts: %d)' %
+                (num_iter, num_restarts))
     min_loss = 0.0
     min_model = None
     for i in range(num_restarts):
-        new_model = GPR_TYPE(X,y,likelihood)
-        loss_i = _run_fit_gpr(new_model,likelihood, X, y, num_iter,debug_log_interval)
+        new_model = GPR_TYPE(X, y, likelihood)
+        logger.debug(new_model.covariance_state_string())
+        loss_i = _run_fit_gpr(new_model, likelihood, X, y, num_iter,
+                              debug_log_interval)
         if loss_i < min_loss or min_model is None:
             logger.debug('Found new best loss: %.3f' % loss_i)
             min_model = new_model
         else:
             logger.debug('Did not find new best loss')
+        logger.debug(new_model.covariance_state_string())
     return min_model
+
 
 def _run_fit_gpr(model, likelihood, X, y, num_iter, debug_log_interval):
     model.train()
     likelihood.train()
     eps = 1e-4
-    h = 100
+    h = 50
 
     # Use the adam optimizer
     optimizer = opt_tp.FullBatchLBFGS(model.parameters())
